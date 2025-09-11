e12214fd730fb6f9d60fbca22d28765400569870
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-09-11 15:31:08 +0200

    Add Config to disable MRPGC Skill Tree Changes

--- src/main/java/com/mrpgc_skilltree/MRPGCSkillTreeAddOn.java
+++ src/main/java/com/mrpgc_skilltree/MRPGCSkillTreeAddOn.java
@@ -3,7 +3,11 @@ package com.mrpgc_skilltree;
 import com.mrpgc_skilltree.effect.MrpgSkillEffects;
 import com.mrpgc_skilltree.skills.CustomSpellImpacts;
 import net.fabricmc.api.ModInitializer;
-
+import com.mrpgc_skilltree.config.TweaksConfig;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 import net.spell_engine.api.config.ConfigFile;
 import net.tinyconfig.ConfigManager;
 import org.slf4j.Logger;
@@ -18,11 +22,27 @@ public class MRPGCSkillTreeAddOn implements ModInitializer {
 			.setDirectory(MOD_ID)
 			.sanitize(true)
 			.build();
+	public static ConfigManager<TweaksConfig> tweaksConfig = new ConfigManager<>
+			("tweaks", new TweaksConfig())
+			.builder()
+			.setDirectory(MOD_ID)
+			.sanitize(true)
+			.build();
 
 
 	@Override
 	public void onInitialize() {
 		effectConfig.refresh();
+		tweaksConfig.refresh();
+		if (MRPGCSkillTreeAddOn.tweaksConfig.value.disable_mrpgc_skilltree_changes) {
+			FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+				ResourceManagerHelper.registerBuiltinResourcePack(
+						Identifier.of(MOD_ID, "mrpgc_skill_tree_changes"),
+						modContainer,
+						ResourcePackActivationType.ALWAYS_ENABLED
+				);
+			});
+		}
 		CustomSpellImpacts.registerCustomImpacts();
 		MrpgSkillEffects.register(effectConfig.value);
 		effectConfig.save();
---
+++ src/main/java/com/mrpgc_skilltree/config/TweaksConfig.java
@@ -0,0 +1,5 @@
+package com.mrpgc_skilltree.config;
+
+public class TweaksConfig {
+    public boolean disable_mrpgc_skilltree_changes = true;
+}
